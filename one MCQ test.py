21f1b21ee31bd5e3ec19a36fa34b277b43445dc7
Lalit Kumar <linkwithlalit@gmail.com>
2023-07-07 18:00:50 +0530

    One Multiple choice question
    Multiple choice question...How to scale it to numerous MCQ exam

---
+++ one MCQ test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Multiple choice question:
+
+#Displaying question and options:
+print("Which of the following is a continent?")
+
+print("1. India\n2. Russia\n3. Canada\n4. Australia")
+
+#Taking input:
+ans = input("Please enter the option number: ")
+#Converting data type of ans from string to int
+ans = int(ans)
+if ans==4:
+    print("Your answer is correct. Well done!")
+else:
+    print("Wrong answer.")
+
+
+# In[ ]:
+
+
+
+
